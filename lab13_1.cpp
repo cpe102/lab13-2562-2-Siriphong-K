71a5b85cae0150ba27c1d4f6008c550112c0f0be
lab314-54@labeng.local <lab314-54@labeng.local>
2020-02-11 17:48:33 +0700

    2516514

--- lab13_1.cpp
+++ lab13_1.cpp
@@ -11,11 +11,48 @@ void swap(T d[],int x,int y){
 
 template <typename T>
 void insertionSort(T d[],int N){
-	
 //Write your code here.
-
+	for(int i=1;i<N;i++){
+		for(int k=0;k<N;k++){
+			if(k==i){
+				cout<<"{"<<d[k]<<"}";
+			}else{
+				cout<<d[k]<<" ";
+			}
+			
+			
+		}
+		cout<< "=>";
+		int L;
+		for(int j=i;j>0;j--){
+			if(d[j]>d[j-1]){
+				swap(d,j,j-1);
+				
+			}		
+		}
+		for(int k=0;k<N;k++){
+			if(k==k-2){
+				cout<<"{"<<d[k]<<"}";
+			}else{
+				cout<<d[k]<<" ";
+			}
+										
+		}
+		cout<<"\n";	
+//	why 0 become 1 for this code 
+//for(int o=N;o>1;o--){
+//	for(int i=0;i<N;i++){
+//		if(d[i]<d[i+1]){
+//			swap(d,i,i+1);
+//		}
+////		if(d[i]==1)d[i]=0;
+//	}
+//	for(int j=0;j<N;j++)cout<<d[j]<<" ";
+//	cout<<"\n";
+//}
+	
+	}
 }
-
 int main(){
 	int a[10] = {12,25,30,44,2,0,4,7,55,25};
 	cout << "Input Array:";
--- lab13_2.cpp
+++ lab13_2.cpp
@@ -21,3 +21,44 @@ int main(){
 }
 
 // Write definition of inputMatrix(),matrixMultiply() and showMatrix() here
+void inputMatrix(double A[][N]){
+//	A[N][N]={1,2,0,3,5,
+//			0,8,7,-1,2,
+//			4,4,3,5,1,
+//			0,0,1,6,0,
+//			1,2,3,4,5}
+	for(int i=0;i<N;i++){
+		cout<<"Row "<<i+1<<":";
+		for(int j=0;j<N;j++){
+		cin>>A[i][j];	
+		}
+
+	}
+
+}
+void findLocalMax(const double A[][N], bool B[][N]){
+	for(int k=0;k<N;k++){
+		B[0][k]=0;
+		B[k][0]=0;
+		B[N-1][k]=0;
+		B[k][N-1]=0;
+	}
+for(int i=1;i<N-1;i++){
+		for(int j=1;j<N-1;j++){
+			if(A[i][j]>A[i-1][j]&&A[i][j]>A[i][j+1]&&A[i][j]>A[i+1][j]&&A[i][j]>A[i][j-1]){
+				B[i][j]=1;
+			}
+			else{
+				B[i][j]=0;
+			}
+}
+}
+}
+void showMatrix(const bool B[][N]){
+	for(int i=0;i<N;i++){
+		for(int j=0;j<N;j++){
+			cout<<B[i][j]<<" ";
+		}
+		cout<<"\n";
+	}
+}
